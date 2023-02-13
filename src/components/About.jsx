94b3c04c409f936809136e0eeafb274db19527ea
Bernardo Branco <bernardomspbranco@gmail.com>
2023-02-13 18:49:29 +0000

    initial commit, home route and about text

---
+++ src/components/About.jsx
@@ -0,0 +1,29 @@
+export default function About() {
+
+  return(
+    <div>
+      <p>
+        Hello! My name is Bernardo Branco, I'm located in Portugal and I love
+        coding, especially in the web development world. My interest started
+        through a friend who said that I'd probably enjoy it, and as expected,
+        he was right.
+      </p>
+      <p>
+        I started learning frontend on my own and it quickly became my
+        day-to-day, soon after that I enrolled in and completed Le Wagon's
+        fullstack bootcamp. It opened my eyes to the fullstack world and I love
+        every part of it. Now I try to improve on the things I already know and
+        keep learning new ones.
+      </p>
+      <p>
+        Here are some of the technologies I've been working on:
+        <ul>
+          <li>JavaScript (ES6+)</li>
+          <li>React</li>
+          <li>Node.js</li>
+          <li>Express.js</li>
+        </ul>
+      </p>
+    </div>
+  )
+}
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import About from "./About";
+
+export default function Home() {
+
+  return (
+    <div className="container">
+      <h1>Hi, my name is Bernardo Branco</h1>
+      <p>I'm a fullstack developer. Focused on learning and becoming the best
+      version of myself</p>
+      <About />
+    </div>
+  )
+}
